1f5f353e09407edea85d2eecfabb96cd8b1fa8fa
IM-GYURI <kgulr0517@gmail.com>
2023-11-13 03:53:12 +0900

    feat: start() 수정

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,23 +1,33 @@
 package christmas.controller;
 
+import static christmas.domain.Menu.createMenu;
+
+import christmas.domain.MenuCategory;
+import christmas.util.OrderCalculator;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.util.List;
 import java.util.Map;
 
 public class EventController {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private int date;
+    private List<MenuCategory> menu = createMenu();
+    private Map<String, Integer> orderMap;
+
+    public EventController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
     public void start() {
-        int date;
-        Map<String, Integer> orderMap;
-
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-
         date = inputView.readDate();
         orderMap = inputView.readOrder();
 
-
-        outputView.printEventPreview(date, orderMap);
-        System.out.println();
+        outputView.printEventPreview(date);
         outputView.printMenu(orderMap);
+        outputView.printTotalPrice(orderMap, menu);
     }
 }
