8f8a72c84049ff1c9aac9b40eda83e9801ea80e9
IM-GYURI <kgulr0517@gmail.com>
2023-11-13 03:54:26 +0900

    feat: printTotalPrice() 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,18 @@
 package christmas.view;
 
+import static christmas.domain.Menu.createMenu;
+
+import christmas.domain.MenuCategory;
+import christmas.util.OrderCalculator;
+import java.text.DecimalFormat;
+import java.util.List;
 import java.util.Map;
 
 public class OutputView {
-    public static void printEventPreview(int day, Map<String, Integer> orderMap) {
+
+    public static void printEventPreview(int day) {
         System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
     }
 
     public void printMenu(Map<String, Integer> orderMap) {
@@ -15,5 +23,15 @@ public class OutputView {
 
             System.out.println(menu + " " + quantity + "개");
         }
+        System.out.println();
+    }
+
+    public void printTotalPrice(Map<String, Integer> orderMap, List<MenuCategory> menu) {
+        int totalPrice = OrderCalculator.calculateTotalPrice(orderMap, menu);
+        DecimalFormat df = new DecimalFormat("###,###");
+        String formatPrice = df.format(totalPrice);
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.println(formatPrice + "원");
+        System.out.println();
     }
 }
