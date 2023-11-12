c7dee31eeddd2737fad04cbb8a05ca4f353e5b0d
IM-GYURI <kgulr0517@gmail.com>
2023-11-13 03:54:52 +0900

    feat: OrderCalculator 작성

---
+++ src/main/java/christmas/util/OrderCalculator.java
@@ -0,0 +1,26 @@
+package christmas.util;
+
+import christmas.domain.MenuCategory;
+import christmas.domain.MenuItem;
+import java.util.List;
+import java.util.Map;
+
+public class OrderCalculator {
+    public static int calculateTotalPrice(Map<String, Integer> orderMap, List<MenuCategory> menu) {
+        int totalPrice = 0;
+
+        for (Map.Entry<String, Integer> entry : orderMap.entrySet()) {
+            String menuItemName = entry.getKey();
+            int quantity = entry.getValue();
+
+            for (MenuCategory category : menu) {
+                for (MenuItem item : category.getItems()) {
+                    if (item.getName().equals(menuItemName)) {
+                        totalPrice += item.getPrice() * quantity;
+                    }
+                }
+            }
+        }
+        return totalPrice;
+    }
+}
