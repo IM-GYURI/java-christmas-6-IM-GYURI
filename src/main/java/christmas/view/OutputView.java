0666c589c14855d1dab0677fae93ba64c0d16a29
IM-GYURI <kgulr0517@gmail.com>
2023-11-13 03:01:14 +0900

    feat: OutputView 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,19 @@
 package christmas.view;
 
+import java.util.Map;
+
 public class OutputView {
-    public void printMenu() {
-        System.out.println("<주문 메뉴>");
-        // ...
+    public static void printEventPreview(int day, Map<String, Integer> orderMap) {
+        System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
     }
-    // ...
+
+    public void printMenu(Map<String, Integer> orderMap) {
+        System.out.println("<주문 메뉴>");
+        for (Map.Entry<String, Integer> entry : orderMap.entrySet()) {
+            String menu = entry.getKey();
+            int quantity = entry.getValue();
+
+            System.out.println(menu + " " + quantity + "개");
+        }
+    }
 }
