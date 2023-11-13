e8e94937fab08f5394b942f6f45bcf18a0306437
IM-GYURI <kgulr0517@gmail.com>
2023-11-13 20:35:01 +0900

    feat: int date를 LocalDate로 변경해주는 클래스 작성

---
+++ src/main/java/christmas/util/LocalDateConverter.java
@@ -0,0 +1,12 @@
+package christmas.util;
+
+import java.time.LocalDate;
+
+public class LocalDateConverter {
+    private static final int year = 2023;
+    private static final int month = 12;
+    public static LocalDate convertToLocalDate(int date)  {
+        LocalDate localDate = LocalDate.of(year, month, date);
+        return localDate;
+    }
+}
