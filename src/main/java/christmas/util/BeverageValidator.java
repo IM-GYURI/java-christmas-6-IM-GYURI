534b81bac1a31a23836908fa3e608f5cf3e00a71
IM-GYURI <kgulr0517@gmail.com>
2023-11-14 04:48:12 +0900

    fix: NullPointerException 오류 해결

--- src/main/java/christmas/util/BeverageValidator.java
+++ src/main/java/christmas/util/BeverageValidator.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 public class BeverageValidator {
-    private static MenuCategory category;
 
     public static boolean containsOnlyBeverages(Map<String, Integer> orderMap, List<MenuCategory> menu) {
         return orderMap.keySet().stream().allMatch(menuName -> isBeverage(menuName, menu));
@@ -13,7 +12,8 @@ public class BeverageValidator {
 
     private static boolean isBeverage(String menuName, List<MenuCategory> menu) {
         return menu.stream()
+                .filter(category -> category.getCategoryName().equals("<음료>"))
                 .flatMap(category -> category.getItems().stream())
-                .anyMatch(item -> item.getName().equals(menuName) && category.isBeverageCategory());
+                .anyMatch(item -> item.getName().equals(menuName));
     }
 }
