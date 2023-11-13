8cecd04e2b84d5b5cdbf93c72628eca7aebcdd32
IM-GYURI <kgulr0517@gmail.com>
2023-11-14 04:49:09 +0900

    feat: ERROR 상수 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.EventController;
 
 public class Application {
+    private static final String ERROR = "[ERROR] ";
     public static void main(String[] args) {
         try {
             EventController eventController = new EventController();
             eventController.start();
         } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.out.println(ERROR  + e.getMessage());
         } finally {
             Console.close();
         }
