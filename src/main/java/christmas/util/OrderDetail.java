3862f44e403e6df7d8b330b2eaacf3e6a5d75aaf
IM-GYURI <kgulr0517@gmail.com>
2023-11-13 20:35:40 +0900

    feat: OrderDetail 클래스 작성

---
+++ src/main/java/christmas/util/OrderDetail.java
@@ -0,0 +1,20 @@
+package christmas.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OrderDetail {
+    private Map<String, Integer> orderMap;
+
+    public OrderDetail() {
+        this.orderMap = new HashMap<>();
+    }
+
+    public void addMenu(String menu, int quantity) {
+        orderMap.merge(menu, quantity, Integer::sum);
+    }
+
+    public Map<String, Integer> getOrderMap() {
+        return orderMap;
+    }
+}
