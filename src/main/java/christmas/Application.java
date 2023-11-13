d1ef17cc7106fa85f59cef72f7f54f982ceec614
IM-GYURI <kgulr0517@gmail.com>
2023-11-14 04:47:31 +0900

    feat: try-catch-finally 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,17 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.EventController;
 
 public class Application {
     public static void main(String[] args) {
-        EventController eventController = new EventController();
-        eventController.start();
+        try {
+            EventController eventController = new EventController();
+            eventController.start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            Console.close();
+        }
     }
 }
