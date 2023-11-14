673bc878f56d791f84683aab95b3c34889eedf01
IM-GYURI <kgulr0517@gmail.com>
2023-11-14 20:10:05 +0900

    style: 컨트롤러명 변경

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,13 +1,13 @@
 package christmas;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.controller.EventController;
+import christmas.controller.OrderController;
 
 public class Application {
     private static final String ERROR = "[ERROR] ";
     public static void main(String[] args) {
         try {
-            EventController eventController = new EventController();
+            OrderController eventController = new OrderController();
             eventController.start();
         } catch (IllegalArgumentException e) {
             System.out.println(ERROR  + e.getMessage());
